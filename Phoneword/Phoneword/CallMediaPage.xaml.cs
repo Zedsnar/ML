d090e3bf9daadd966437dc3d4044007ae026b5d1
Micha <>
2018-09-10 17:41:06 +0200

    Page für Camera und Gallery hinzugefügt

---
+++ Phoneword/Phoneword/CallMediaPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class CallMediaPage : ContentPage
+    {
+        public CallMediaPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnGallery(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new CallFibonacciPage());
+        }
+
+        async void OnCamera(object sender, EventArgs e)
+        {
+            //await Navigation.PushAsync(new CallFibonacciPage());
+        }
+    }
+}
